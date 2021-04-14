858cc2d0e00578047c7c99210fdcdba3515b0446
Jonas Oliveira <jonascsoliveira@outlook.com>
2021-04-14 12:07:52 -0300

    Preparando para gerar dist

--- src/app.js
+++ src/app.js
@@ -3,14 +3,14 @@ import { resolve } from 'path';
 
 dotenv.config();
 
-import './src/database';
+import './database';
 import express from 'express';
 
-import homeRoutes from './src/routes/homeRoutes';
-import userRoutes from './src/routes/userRoutes';
-import tokenRoutes from './src/routes/tokenRoutes';
-import alunoRoutes from './src/routes/alunoRoutes';
-import fileRoutes from './src/routes/fileRoutes';
+import homeRoutes from './routes/homeRoutes';
+import userRoutes from './routes/userRoutes';
+import tokenRoutes from './routes/tokenRoutes';
+import alunoRoutes from './routes/alunoRoutes';
+import fileRoutes from './routes/fileRoutes';
 
 class App {
   constructor() {
